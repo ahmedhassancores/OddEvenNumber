0a036f38b89a4be08041ae2d6dcf13ce7eada840
Ahmed Hassan(Dev) <a.hassan@arco.sa>
2023-05-07 16:28:14 +0300

    initial commit

---
+++ OddEvenNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
